f80f6dcd8ddd11d18dd989259aa2b305ee052276
Megan Ford <meg.c.ford@gmail.com>
2016-09-13 17:37:39 -0500

    removed a lot of unneccessary files and updated bower.json.

--- README.md
+++ README.md
@@ -1,5 +1,20 @@
-# BaconIsBetter
-Small site built as a favor to a friend who likes bacon
+# Bacon Is Better
 
-npm init
-bower init
+Small site (3 hour timeline) built as a favor to a friend who likes bacon. Is anything better than bacon?
+
+Deployed at []()
+
+---------
+## Install 
+
+```
+$ git clone https://github.com/MeganCFord/BaconIsBetter.git
+$ cd BaconIsBetter
+$ bower install  
+
+```
+serve on server of choice.
+
+-----------
+
+#### License: [MIT](LICENSE.md)
--- main.js
+++ main.js
@@ -5,22 +5,22 @@ $("#submitButton").click(function(){
 
   var worseThing = $("#anything").val();
 
-  var worseSentence = `Bacon is better than ${worseThing}.`
+  var worseSentence = `Bacon is better than ${worseThing}.`;
 
   if (worseThing.toUpperCase() === "BACON") {
 
     $("#answer").text("BACON.");
     $("#sentence").text("Bacon bacon, bacon bacon bacon bacon bacon.");
-    $(".title").text("Bacon Bacon Bacon?")
+    $(".title").text("Bacon Bacon Bacon?");
     $("#submitButton").text("Bacon");
-    $("#refreshButton").text("Bacon Bacon Bacon ^")
+    $("#refreshButton").text("Bacon Bacon Bacon ^");
     $("#question").text("Bacon Bacon Bacon Bacon:");
 
   } else {
     
     $("#sentence").text(worseSentence);
     
-  };
+  }
 
   //displays the refresh button after the 'yes' displays. 
   $("#answer").slideDown("400", "swing", function(){
@@ -30,7 +30,7 @@ $("#submitButton").click(function(){
       $("#refreshButton").slideDown();
     });
   });
-  $("#submitButton").attr("disabled", "disabled")
+  $("#submitButton").attr("disabled", "disabled");
   
 });
 
@@ -48,14 +48,14 @@ $("#refreshButton").on("click", function(){
 
   $(".title").text("Is Bacon Better?");
   $("#submitButton").text("Submit");
-  $("#refreshButton").text("try Something Else")
+  $("#refreshButton").text("try Something Else");
   $("#answer").text("Yes.");
-  $("#question").text("Is Bacon Better Than:")
+  $("#question").text("Is Bacon Better Than:");
 
   $("#refreshButton").slideUp();
   $("#sentence").slideUp();
   $("#answer").slideUp();
   $("#anything").val("");
   $("#anything").focus();
-  $("#submitButton").prop("disabled", false)
-})
+  $("#submitButton").prop("disabled", false);
+});
